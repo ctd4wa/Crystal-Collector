dd55265209c6ce967247013d2002ea1dc10d8659
Chris Duong <ctd4wa@virginia.edu>
2019-06-18 13:01:19 -0400

    adding some notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var Generated = Math.floor(Math.random()*101+19)
     $('#randomNumber').html(Generated);
 
+    // numbers randomly generated between 1-12 for each gem
     var red = Math.floor(Math.random()*11+1);
     var orange = Math.floor(Math.random()*11+1);
     var blue = Math.floor(Math.random()*11+1);
@@ -11,5 +12,9 @@ $(document).ready(function(){
     var PlayerTotal = 0;
     var wins = 0;
     var losses = 0;
+
+    //  add gem scores for each click on the gems
+    //  append the values to each other to generate the player total
+    //  reset the random number and each individual gem values
 
 })
