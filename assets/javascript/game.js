2ae0c4397f6b7098fc9fa46e779fdd7a4d948543
Chris Duong <ctd4wa@virginia.edu>
2019-06-17 23:22:06 -0400

    checking out for the night

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var Generated = Math.floor(Math.random()*101+19)
+    $('#randomNumber').html(Generated);
+
+    var red = Math.floor(Math.random()*11+1);
+    var orange = Math.floor(Math.random()*11+1);
+    var blue = Math.floor(Math.random()*11+1);
+    var green = Math.floor(Math.random()*11+1);
+
+    var PlayerTotal = 0;
+    var wins = 0;
+    var losses = 0;
+})
