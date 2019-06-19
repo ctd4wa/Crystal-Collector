e0a6aab45790bdc7efd6ad0b68e1dcc9d43c44ed
Chris Duong <ctd4wa@virginia.edu>
2019-06-18 21:04:18 -0400

    working out the kinks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,83 @@ $(document).ready(function(){
     var wins = 0;
     var losses = 0;
 
+    $('#Wins').text(wins);
+    $('#Losses').text(losses);
+    $('UserTotal').text(PlayerTotal);
+
+    function reset(){
+        Generated = Math.floor(Math.random()*101+19);
+        console.log(Generated)
+        $('#randomNumber').text(Generated);
+        red = Math.floor(Math.random()*11+1);
+        orange = Math.floor(Math.random()*11+1);
+        blue = Math.floor(Math.random()*11+1);
+        green = Math.floor(Math.random()*11+1);
+        PlayerTotal = 0;
+        $('UserTotal').text(PlayerTotal);
+    }
+
+    function winner(){
+        alert("Well done!");
+        wins++;
+        $('#Wins').text(wins);
+        reset();
+    }
+
+    function loser(){
+        alert("Oh no!");
+        losses++;
+        $('#Losses').text(losses);
+        reset()
+    }
+
+    $('#Sun').on('click',function(){
+        PlayerTotal = PlayerTotal + red;
+        console.log("New UserTotal = " + PlayerTotal);
+        $('UserTotal').text(PlayerTotal);
+            if (PlayerTotal == Generated) {
+                winner();
+            }
+            else if (PlayerTotal > Generated) {
+                loser();
+            }
+    });
+
+    $('#Fire').on('click',function(){
+        PlayerTotal = PlayerTotal + orange;
+        console.log("New UserTotal = " + PlayerTotal);
+        $('UserTotal').text(PlayerTotal);
+            if (PlayerTotal == Generated) {
+                winner();
+            }
+            else if (PlayerTotal > Generated) {
+                loser();
+            }
+    });
+
+    $('#Water').on('click',function(){
+        PlayerTotal = PlayerTotal + blue;
+        console.log("New UserTotal = " + PlayerTotal);
+        $('UserTotal').text(PlayerTotal);
+            if (PlayerTotal == Generated) {
+                winner();
+            }
+            else if (PlayerTotal > Generated) {
+                loser();
+            }
+    });
+
+    $('#Thunder').on('click',function(){
+        PlayerTotal = PlayerTotal + green;
+        console.log("New UserTotal = " + PlayerTotal);
+        $('UserTotal').text(PlayerTotal);
+            if (PlayerTotal == Generated) {
+                winner();
+            }
+            else if (PlayerTotal > Generated) {
+                loser();
+            }
+    });
     //  add gem scores for each click on the gems
     //  append the values to each other to generate the player total
     //  if statement: if the Player score === randomNumber, win++
